b0c51dafdc52923f80c398a0466579cf42310d60
Dustin Gaspard <dgaspard@gmail.com>
2021-06-19 19:40:23 -0500

    Update and Delete Checkout Custom Exception Handling

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    class UpdateOrderCommandHandler : IRequest<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<CheckoutOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(_orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(_mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(_logger));
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id);
+            if(orderToUpdate == null)
+            {
+                _logger.LogError("Order does not exist in the database");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+            await _orderRepository.UpdateAsync(orderToUpdate);
+            _logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+            return Unit.Value;
+        }
+    }
+}
